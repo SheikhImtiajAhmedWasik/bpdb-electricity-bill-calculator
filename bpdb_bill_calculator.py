2fe4b67ea79fa0863029679dce0d58205b4c4816
Wasik <sheikhimtiajahmed48@gmail.com>
2026-08-01 23:15:01 +0600

    Complete the bill calculator

--- bpdb_bill_calculator.py
+++ bpdb_bill_calculator.py
@@ -1,1 +1,95 @@
 
+def life_line(units):
+    # Unit 0 to 50 --> price 4.63
+    total_bill = units * 4.63
+    return total_bill
+
+
+def step1(units):
+    # Unit up to 50 --> price 5.26
+    total_bill = units * 5.26
+    return total_bill
+
+
+def step2(units):
+    # Unit up to 75 --> price 7.20
+    total_bill = (units - 75) * 7.20 + step1(75)
+    return total_bill
+
+
+def step3(units):
+    # Unit up to 200 --> price 7.59
+    total_bill = (units - 200) * 7.59 + step2(200)
+    return total_bill
+
+
+def step4(units):
+    # Unit up to 300 --> price 8.02
+    total_bill = (units - 300) * 8.02 + step3(300)
+    return total_bill
+
+
+def step5(units):
+    # Unit up to 400 --> price 12.67
+    total_bill = (units - 400) * 12.67 + step4(400)
+    return total_bill
+
+
+def step6(units):
+    # Unit up to 600 --> price 14.61
+    total_bill = (units - 600) * 14.61 + step5(600)
+    return total_bill
+
+
+def main():
+    # User Input
+    total_units = float(input("Total Units: "))
+    demand_load = int(input("Demand Load in KW(Its generally 1 or 2 in LT-A): "))
+
+    # Output variable
+    total_bill = 0
+    demand_charge = demand_load * 42
+    vat = 0
+    payable_amount = 0
+
+    if total_units > 600:
+        total_bill = step6(total_units)
+
+    elif total_units > 400:
+        total_bill = step5(total_units)
+
+    elif total_units > 300:
+        total_bill = step4(total_units)
+
+    elif total_units > 200:
+        total_bill = step3(total_units)
+
+    elif total_units > 75:
+        total_bill = step2(total_units)
+
+    elif total_units > 50:
+        total_bill = step1(total_units)
+    else:
+        total_bill = life_line(total_units)
+
+
+    vat = total_bill * (5/100)
+    payable_amount = total_bill + demand_charge + vat
+
+    # final output
+    print("="*40)
+    print("\tElectricity Bill")
+    print("="*40)
+    print(f"\nTotal Units: {total_units}\nDemand Load: {demand_load} KW\nEnergy Charge: {total_bill} Tk\nDemand Charge: {demand_charge} Tk\nVat: {vat}")
+    print("-"*40)
+    print(f"Total Amount: {payable_amount} Tk\n")
+    print("="*40)
+    
+
+
+
+main()
+
+
+
+
